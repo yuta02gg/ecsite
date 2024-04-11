501cdce5619d2a249336a0105520d57e16a1fa5b
Your Name <you@example.com>
2024-04-11 19:36:59 +0900

    forth

---
+++ src/com/diworksdev/ecsite/dao/UserCreateCompleteDAO.java
@@ -0,0 +1,33 @@
+package com.diworksdev.ecsite.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.diworksdev.ecsite.util.DBConnector;
+import com.diworksdev.ecsite.util.DateUtil;
+
+public class UserCreateCompleteDAO {
+    private DBConnector dbConnector = new DBConnector();
+    private Connection connection = dbConnector.getConnection();
+    private DateUtil dateUtil = new DateUtil();
+
+    private String sql = "INSERT INTO login_user_transaction (login_id, login_pass, user_name, insert_date) VALUES(?, ?, ?, ?)";
+
+    public void createUser(String loginUserId, String loginUserPassword, String userName) throws SQLException {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, loginUserId);
+            preparedStatement.setString(2, loginUserPassword);
+            preparedStatement.setString(3, userName);
+            preparedStatement.setString(4, dateUtil.getDate());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+}
